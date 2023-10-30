d9953ba1498fd068757a4725e880d1db6661b05e
Alexander Zibert <alexander.zibert@googlemail.com>
2023-10-30 17:14:29 +0100

    remove correct state from digitCodeSlice

--- frontend/src/store/slices/digitCodeSlice.ts
+++ frontend/src/store/slices/digitCodeSlice.ts
@@ -1,15 +1,15 @@
-import { PayloadAction, createSlice } from '@reduxjs/toolkit'
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 
 export type DigitCodeState = {
-  shape: Shape
-  digit: Digit
-  state: 'correct' | 'incorrect'
-}[]
+  shape: Shape;
+  digit: Digit;
+  state: "correct" | "incorrect";
+}[];
 
-const initialState: DigitCodeState = []
+const initialState: DigitCodeState = [];
 
 export const digitCodeSlice = createSlice({
-  name: 'digitCode',
+  name: "digitCode",
   initialState,
   reducers: {
     load: (_, action: PayloadAction<DigitCodeState>) => action.payload,
@@ -18,28 +18,21 @@ export const digitCodeSlice = createSlice({
       state,
       action: PayloadAction<{ shape: Shape; digit: Digit }>
     ) => {
-      const { shape, digit } = action.payload
+      const { shape, digit } = action.payload;
 
       const index = state.findIndex(
-        entry => entry.shape === shape && entry.digit === digit
-      )
+        (entry) => entry.shape === shape && entry.digit === digit
+      );
 
       if (index >= 0) {
-        switch (state[index].state) {
-          case 'incorrect':
-            state[index].state = 'correct'
-            break
-          case 'correct':
-            state.splice(index, 1)
-            break
-        }
+        state.splice(index, 1);
       } else {
-        state.push({ shape, digit, state: 'incorrect' })
+        state.push({ shape, digit, state: "incorrect" });
       }
     },
   },
-})
+});
 
-export const digitCodeActions = digitCodeSlice.actions
+export const digitCodeActions = digitCodeSlice.actions;
 
-export default digitCodeSlice.reducer
+export default digitCodeSlice.reducer;
