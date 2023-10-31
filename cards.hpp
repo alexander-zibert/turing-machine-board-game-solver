47af812dac27de5339d9d835512fa2d71105638f
Alexander Zibert <alexander.zibert@googlemail.com>
2023-10-31 08:10:49 +0100

    fix invalid type

--- cards.hpp
+++ cards.hpp
@@ -508,8 +508,8 @@ const auto c48 = card_t{blue_less_than_yellow,     blue_equal_to_yellow,
                         yellow_less_than_purple,   yellow_equal_to_purple,
                         yellow_greater_than_purple};
 
-const auto all_cards = {{},  c1,  c2,  c3,  c4,  c5,  c6,  c7,  c8,  c9,
-                        c10, c11, c12, c13, c14, c15, c16, c17, c18, c19,
-                        c20, c21, c22, c23, c24, c25, c26, c27, c28, c29,
-                        c30, c31, c32, c33, c34, c35, c36, c37, c38, c39,
-                        c40, c41, c42, c43, c44, c45, c46, c47, c48};
+const auto all_cards =
+    std::vector{{},  c1,  c2,  c3,  c4,  c5,  c6,  c7,  c8,  c9,  c10, c11, c12,
+                c13, c14, c15, c16, c17, c18, c19, c20, c21, c22, c23, c24, c25,
+                c26, c27, c28, c29, c30, c31, c32, c33, c34, c35, c36, c37, c38,
+                c39, c40, c41, c42, c43, c44, c45, c46, c47, c48};
