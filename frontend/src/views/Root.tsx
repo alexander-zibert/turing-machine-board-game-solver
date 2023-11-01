bbb05f4a2eed6f3d881e8f860d6b79100cbb10cc
Alexander Zibert <alexander.zibert@googlemail.com>
2023-11-01 12:17:59 +0100

    add link to my github

--- frontend/src/views/Root.tsx
+++ frontend/src/views/Root.tsx
@@ -199,7 +199,7 @@ const Root: FC = () => {
             </IconButton>
             <IconButton
               aria-label="github"
-              href="https://github.com/accuzyle/turing-machine-interactive-sheet"
+              href="https://github.com/alexander-zibert/turing-machine-board-game-solver"
               target="_blank"
             >
               <GitHubIcon />
