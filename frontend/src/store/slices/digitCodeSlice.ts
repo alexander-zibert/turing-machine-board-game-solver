208d20f807a6262087efd896c1c9c0bd06ad92ee
Frank R <github@fr-net.de>
2023-12-22 19:32:48 +0100

    remove toggleDigitState

--- frontend/src/store/slices/digitCodeSlice.ts
+++ frontend/src/store/slices/digitCodeSlice.ts
@@ -14,22 +14,6 @@ export const digitCodeSlice = createSlice({
   reducers: {
     load: (_, action: PayloadAction<DigitCodeState>) => action.payload,
     reset: () => initialState,
-    toggleDigitState: (
-      state,
-      action: PayloadAction<{ shape: Shape; digit: Digit }>
-    ) => {
-      const { shape, digit } = action.payload;
-
-      const index = state.findIndex(
-        (entry) => entry.shape === shape && entry.digit === digit
-      );
-
-      if (index >= 0) {
-        state.splice(index, 1);
-      } else {
-        state.push({ shape, digit, state: "incorrect" });
-      }
-    },
     setDigitState: (
       state,
       action: PayloadAction<{ shape: Shape; digit: Digit; isValid: boolean }>
